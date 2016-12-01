0d799a6dc796950cc6a1dbcc6aa50cc92300db6f
xiajiangtao <771576065@qq.com>
2016-12-01 10:55:01 +0800

    UEditor

---
+++ project/UEditor/UEditor/list.js
@@ -0,0 +1,153 @@
+var docList = [
+    {
+        id: 'start',
+        title: '1. 开始使用',
+        list: [
+            {
+                id:'start',
+                title: '1.1ueditor'
+            },
+            {
+                id:'toolbar',
+                title: '1.2 定制工具栏图标'
+            },
+            {
+                id: 'config',
+                title: '1.3 前端配置项说明'
+            },
+            {
+                id: 'dir',
+                title: '1.4 目录介绍'
+            },
+            {
+                id: 'submit',
+                title: '1.5 提交表单'
+            },
+            {
+                id: 'uparse',
+                title: '1.6 编辑内容展示'
+            },
+            {
+                id: 'patch',
+                title: '1.7 如何使用补丁文件'
+            }//,
+//            {
+//                id: 'shortkey',
+//                title: '1.8 设置快捷键'
+//            },
+//            {
+//                id: 'event',
+//                title: '1.9 监听编辑器事件'
+//            },
+//            {
+//                id: 'filter',
+//                title: '1.10 粘贴过滤器(黑白名单)'
+//            }
+        ]
+    },
+    {
+        id: 'server',
+        title: '2. 后端配置',
+        list: [
+            {
+                id:'deploy',
+                title: '2.1 后端部署说明'
+            },
+            {
+                id:'php',
+                title: '2.2 PHP 使用说明'
+            },
+            {
+                id:'asp',
+                title: '2.3 ASP 使用说明'
+            },
+            {
+                id:'net',
+                title: '2.4 ASP.NET 使用说明'
+            },
+            {
+                id:'jsp',
+                title: '2.5 JSP 使用说明'
+            },
+            // {
+            //     id: 'django',
+            //     title: '2.6 Python Django 使用说明'
+            // },
+            {
+                id:'config',
+                title: '2.7 后端配置项说明'
+            },
+            {
+                id:'path',
+                title: '2.8 上传路径配置'
+            },
+            {
+                id: 'video',
+                title: '2.9 上传视频'
+            }
+        ]
+    },
+    {
+        id: 'dev',
+        title: '3. 二次开发',
+        list: [
+            {
+                id: 'developer',
+                title: '3.1 如何进行二次开发'
+            },
+            {
+                id: 'bale_width_grunt',
+                title: '3.2 使用grunt打包源代码'
+            },
+            {
+                id:'request_specification',
+                title: '3.3 后端请求规范'
+            },
+            {
+                id:'serverparam',
+                title: '3.4 自定义请求参数'
+            },
+            {
+                id:'crossdomain',
+                title: '3.5 跨域支持说明'
+            }//,
+//            {
+//                id: '',
+//                title: '3.6 修改编辑区域样式'
+//            },
+//            {
+//                id: '',
+//                title: '3.7 输入输出过滤规则'
+//            },
+        ]
+    },
+    {
+        id: 'api',
+        title: '4. 常用API列表',
+        list: [
+            {
+                id: 'common',
+                title: '4.1 常用API'
+            }
+        ]
+    },
+    {
+        id: 'qa',
+        title: '5. 常见问题',
+        list: [
+            {
+                id:'ready',
+                title:'5.1 实例创建后为什么直接执行接口报错'
+            },
+            {
+                id:'customurl',
+                title:'5.2 如何自定义请求地址'
+            },
+            {
+                id:'allowDivToP',
+                title:'5.3 如何阻止div变成p标签'
+            }
+
+        ]
+    }
+];
